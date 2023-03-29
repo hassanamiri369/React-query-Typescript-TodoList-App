0f198bda40234b308fdc47e2931dc4da051ff1c1
Hassanamiri <hassanamiri369@gmail.com>
2023-03-29 19:42:39 +0330

    adding delete and edit icons

--- app/src/pages/TodoList.tsx
+++ app/src/pages/TodoList.tsx
@@ -2,6 +2,9 @@ import React from 'react'
 import {useQuery} from "@tanstack/react-query";
 import { getTodos } from '../api';
 
+import { FiDelete } from 'react-icons/fi';
+import { CiEdit } from 'react-icons/ci';
+
 
 const TodoList = () => {
 
@@ -15,6 +18,10 @@ const TodoList = () => {
             <p>{item.title}</p>
             <p>{item.body}</p>
             <div>{item.complete ? <p>done</p> : <p>un done</p>}</div>
+            <div>
+                <span className='edit-ico'><CiEdit/></span>
+                <span className='delete-ico'><FiDelete/></span>
+            </div>
         </div>
        ))}
     </div>
