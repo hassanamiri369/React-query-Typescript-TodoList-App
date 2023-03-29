fb26e147247510f8827c7b8141f03fc3c85aa960
Hassanamiri <hassanamiri369@gmail.com>
2023-03-29 18:40:57 +0330

    useMutation hook and useNavigate

--- app/src/components/addTodo/AddTodo.tsx
+++ app/src/components/addTodo/AddTodo.tsx
@@ -1,20 +1,46 @@
 import React , {useState , useEffect} from 'react';
 import { addTodos } from '../../api'
-import {useMutation} from "@tanstack/react-query";
+import {useMutation ,useQueryClient } from "@tanstack/react-query";
+import {useNavigate} from "react-router-dom";
 
 
 
 const AddTodo = () => {
 
+    const queryClient = useQueryClient()
+    const navigate = useNavigate()
+
     // state 
     const [title , setTitle] = useState("")
     const [body , setBody ] = useState("")
 
-    const {data} = useMutation(addTodos)
+    //  post new todo
+    const {data , mutate} = useMutation(addTodos , {
+        onSuccess : ()=>{
+            queryClient.invalidateQueries(['todos'])
+            console.log("suucess adding")
+            console.log(data)
+        }
+    })
+    
+    const handleSubmit = (e : React.FormEvent<HTMLFormElement>) =>{
+        e.preventDefault()
+
+        if(!title) return alert("title must be complete !!!")
+        if(!body) return alert("body must be complete !!! ")
+
+        const newTodo = {id : Number(new Date()) , title , body , complete : false}
+        mutate(newTodo)
+        setTitle("")
+        setBody("")
+        navigate("/")
+
+
+    }
 
   return (
     <div>
-        <form>
+        <form onSubmit={(e)=> handleSubmit(e)}>
             <div className='t-title'>
                 <label>Title</label>
                 <input type='text' value={title} onChange={(e)=> setTitle(e.target.value)} placeholder='title'/>
