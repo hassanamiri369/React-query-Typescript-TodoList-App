4bf093750f268178b956b3120f525004a28672c0
Hassanamiri <hassanamiri369@gmail.com>
2023-03-29 21:35:34 +0330

    get one todo info

--- app/src/pages/TodoDetail.tsx
+++ app/src/pages/TodoDetail.tsx
@@ -1,13 +1,36 @@
+import { useQuery } from '@tanstack/react-query'
 import React from 'react'
-import { Outlet, useParams } from 'react-router-dom'
+import { Outlet, useNavigate, useParams } from 'react-router-dom'
+import { getOneTodo } from '../api'
 
 const TodoDetail = () => {
     const {id} = useParams()
+
+    const navigate = useNavigate()
+
+    const {data , isLoading} = useQuery({
+      queryKey : ['todos' , id],
+      queryFn : ()=> getOneTodo(id),
+      onSuccess : ()=>{console.log(data)}
+    }
+      )
+
+      
 
   return (
     <>
         <Outlet/>
         <div>{id}</div>
+        <div>
+          {isLoading ? <div>loading ...</div> : null}
+          <div>
+            <button onClick={()=> navigate("/")}>go back home</button>
+          </div>
+          {data?.data && <div>
+              <p>{data.data.title}</p>
+              <pre>{data.data.body}</pre>
+            </div>}
+        </div>
     </>
   )
 }
