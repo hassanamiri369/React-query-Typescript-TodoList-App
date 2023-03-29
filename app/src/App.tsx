b8765e46fe80f489fab89525e378094c740b6046
Hassanamiri <hassanamiri369@gmail.com>
2023-03-29 14:29:41 +0330

    first commit - setup basic project

---
+++ app/src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+        <h2>TodoList App - React-Query & Typescript</h2>
+    </div>
+  )
+}
+
+export default App
