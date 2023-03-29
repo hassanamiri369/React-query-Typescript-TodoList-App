36e92be9586146b32462b96fbe6a96cc1d479185
Hassanamiri <hassanamiri369@gmail.com>
2023-03-29 16:09:42 +0330

    adding todoList component

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,12 +1,17 @@
 import { useState } from 'react'
+import TodoList from './components/TodoList/TodoList'
 
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <div className="App">
         <h2>TodoList App - React-Query & Typescript</h2>
+
+        <div>
+          <TodoList/>
+        </div>
     </div>
   )
 }
