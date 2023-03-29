54ca391db0d93a145c11ce0788c5bbe3e6598cb2
Hassanamiri <hassanamiri369@gmail.com>
2023-03-29 21:52:48 +0330

    create get one todo api

--- app/src/api/index.tsx
+++ app/src/api/index.tsx
@@ -17,6 +17,11 @@ export const getTodos  = async ()   =>{
     return response;
 }
 
+export const getOneTodo = async(id : any)=>{
+    const response = await api.get(`/todos/${id}`);
+    return response
+}
+
 
 export const addTodos = async(todo : ITodo)=>{
     const response = await api.post('/todos' , todo);
