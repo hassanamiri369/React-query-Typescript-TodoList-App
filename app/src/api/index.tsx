3a96122513dcaf1be3ba13b90fa6eaac571dcaed
Hassanamiri <hassanamiri369@gmail.com>
2023-03-29 16:09:28 +0330

    create ITodo type

--- app/src/api/index.tsx
+++ app/src/api/index.tsx
@@ -4,9 +4,21 @@ const api = axios.create({
     baseURL : "http://localhost:8000"
 })
 
+export interface ITodo  {
+    id : number;
+    title : string;
+    body : string;
+    complete : boolean
 
+}
 
-export const getTodos = async()=>{
-    const response = await api.get('/todos')
+export const getTodos  = async ()   =>{
+    const response = await api.get <ITodo[]>("/todos")
     return response
 }
+
+
+// export const addTodos = async(todo : ITodo)=>{
+//     const response = await api.post('/todos' , todo)
+//     return response
+// }
