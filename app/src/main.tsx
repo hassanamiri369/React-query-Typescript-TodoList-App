a9d928df93a2f47c4faa3bb77d72ef08899261f7
Hassanamiri <hassanamiri369@gmail.com>
2023-03-29 14:45:02 +0330

    set up react query provider

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -3,8 +3,23 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
+// config react query 
+import {
+  useQuery,
+  useMutation,
+  useQueryClient,
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query';
+
+
+const queryClient =new  QueryClient()
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
     <App />
+    </QueryClientProvider>
+   
   </React.StrictMode>,
 )
