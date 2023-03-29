59d17eca9e00ccea1cf5685412c5df379f6dd471
Hassanamiri <hassanamiri369@gmail.com>
2023-03-29 18:41:10 +0330

    create routes

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,17 +1,42 @@
 import { useState } from 'react'
-import TodoList from './components/TodoList/TodoList'
+import TodoList from './pages/TodoList';
+
+import { Routes, Route , Link } from "react-router-dom";
+import AddTodo from './components/addTodo/AddTodo';
+import TodoDetail from './pages/TodoDetail';
+
+
 
 
 function App() {
-  
+
 
   return (
     <div className="App">
+      <header>
         <h2>TodoList App - React-Query & Typescript</h2>
+      </header>
 
-        <div>
-          <TodoList/>
-        </div>
+      <main>
+        <section>
+          <div className='links-container'>
+            <Link to={'/addTodo'}>NEW TODO</Link>
+          </div>
+        </section>
+
+
+        <section>
+          <div className='routes-container'>
+            <Routes>
+              <Route path='/' element={<TodoList />} />
+              <Route path='/todoDetail/:id' element={<TodoDetail/>}/>
+              <Route path='/addTodo' element={<AddTodo />} />
+            </Routes>
+          </div>
+        </section>
+      </main>
+
+
     </div>
   )
 }
