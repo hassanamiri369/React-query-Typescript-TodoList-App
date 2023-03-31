8c124f11cc2d03032c2028959840bfe148fe2c7e
Hassanamiri <hassanamiri369@gmail.com>
2023-03-31 16:28:04 +0330

    adding todo status

--- app/src/pages/TodoList.tsx
+++ app/src/pages/TodoList.tsx
@@ -1,7 +1,7 @@
-import React , {useState} from 'react'
+import React, { useState } from 'react'
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 
-import { ITodo, deleteTodo, getTodos } from '../api';
+import { ITodo, deleteTodo, getAllTodos, getTodos } from '../api';
 
 import { FiDelete } from 'react-icons/fi';
 import { CiEdit, CiSearch } from 'react-icons/ci';
@@ -13,27 +13,31 @@ import { AiOutlineClose } from 'react-icons/ai';
 
 const TodoList = () => {
 
-    const [page , setPage] = useState(1)
+    const [page, setPage] = useState(1)
 
-        const NextHandler = ()=> {
-            setPage(prev => prev + 1)
-        }
+    const NextHandler = () => {
+        setPage(prev => prev + 1)
+    }
 
-        const PrevHandler = ()=> {
-            setPage(prev => prev - 1)
-        }
+    const PrevHandler = () => {
+        setPage(prev => prev - 1)
+    }
 
     const navigate = useNavigate()
     const queryClient = useQueryClient();
 
-    const { data, isLoading } = useQuery(['todos' , page], ()=>getTodos(page))
+    const { data, isLoading } = useQuery(['todos', page], () => getTodos(page))
+    const { data : todos } = useQuery(['todos'], getAllTodos)
 
+    const processing = data?.data.filter(item => item.complete ==="Done")
+    const complete = data?.data.filter(item => item.complete ==="unDone")
+    
     const { mutate } = useMutation(deleteTodo, {
         onSuccess: () => {
             queryClient.invalidateQueries(['todos'])
             console.log("delte todo success")
             toast.error("delete todo success !!", { position: "top-center" })
-           
+
         }
     })
 
@@ -42,9 +46,9 @@ const TodoList = () => {
         mutate(id)
     }
 
-    
 
-    
+
+
 
 
     return (
@@ -57,29 +61,36 @@ const TodoList = () => {
                     <Link to={'/addTodo'}>NEW TODO</Link>
                 </div>
 
-               
+
             </section>
 
             <section className='pagination-container'>
-               <div className='page-content'>
-               <button disabled={page === 1} onClick={PrevHandler}>prev</button>
-                <span>page</span>{"  "}<span style={{fontWeight : "bold"}}>{page}</span>
-                <button disabled={page === data?.headers["x-total-count"] /2} onClick={NextHandler}>next</button>
-               </div>
+                <div className='page-content'>
+                    <button disabled={page === 1} onClick={PrevHandler}>prev</button>
+                    <span>page</span>{"  "}<span style={{ fontWeight: "bold" }}>{page}</span>
+                    <button disabled={page === data?.headers["x-total-count"] / 2} onClick={NextHandler}>next</button>
+                </div >
+                    
+                <div className='status'>
+                    <h3>Status : </h3>
+                    <p><span>Count Todo</span><span>{todos?.data.length}</span></p>
+                    <p><span>Processing</span><span>{processing&& processing.length}</span></p>
+                    <p><span>Complete</span><span>{complete && complete.length}</span></p>
+                </div>
             </section>
 
 
             <section className='show-item'>
                 {data?.data.map((item) => (
 
-                    <div className='content' key={item.id}>
+                    <div  className={`content ${item.complete === "Done" ? "completeTodo" : "proccessingTodo"}`} key={item.id}>
                         <p className='c-title'>{item.title}</p>
                         <div>{item.complete}</div>
                         <div>
-                        <Link key={item.id} to={`/todoDetail/${item.id}`}> More Detail </Link>
+                            <Link key={item.id} to={`/todoDetail/${item.id}`}> More Detail </Link>
                         </div>
                         <div>
-                           
+
                             <span title='edit' className='edit-ico'><CiEdit onClick={() => navigate(`/editTodo/${item.id}`)} /></span>
                             <span className='delete-ico'><FiDelete onClick={() => handleDelete(Number(item.id))} /></span>
                         </div>
